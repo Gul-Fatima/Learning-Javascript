b695b3a29ea24e5a6a6271a93e9fb5dd93b38064
Gul Fatima <gulfatima1074@gmail.com>
2025-01-13 08:14:13 -0800

    Added notes of basic LearnWhatMatters

---
+++ Javascript_Mastery/Arrays.js
@@ -0,0 +1,13 @@
+var arr = [1,2,3,4,5,6,7];
+
+arr[0]; //1
+arr[1]; //2
+
+//-----Array functions -----------
+
+arr.push(8); //add element at last index
+arr.pop(); //delete last index element
+arr.unshift(4); //add element at start
+arr.shift(); //remmove element from start
+arr.splice(2,1) //delete 1 elemnt form 2 index i.e 3
+arr.splice(2,3) //delete 3 elemnt form 2 index i.e 3,4,5
---
+++ Javascript_Mastery/Object.js
@@ -0,0 +1,15 @@
+//Hold details of an elemnt like a person so his name,age,....
+var a = {
+    name : 'John',
+    age : 12,
+    id : 3,
+    func_name : function(){
+        console.log(`Hello, I am ${name}. My ag is ${age}.` )
+    }
+
+}
+
+//Access value 
+console.log(a.age);
+console.log(a.id);
+console.log(a.name);
