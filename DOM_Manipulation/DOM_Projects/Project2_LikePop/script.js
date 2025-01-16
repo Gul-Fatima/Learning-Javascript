a1f7cfddcd476a8511b8ac564a159736de27ac41
Gul Fatima <gulfatima1074@gmail.com>
2025-01-16 05:00:23 -0800

    Completed second project

--- DOM_Manipulation/DOM_Projects/Project2_LikePop/script.js
+++ DOM_Manipulation/DOM_Projects/Project2_LikePop/script.js
@@ -1,1 +1,15 @@
+var card = document.querySelector(".card");
+var icon = document.querySelector("i")
 
+card.addEventListener("dblclick", function(){
+    icon.style.transform = 'translate(-50%,-50%) scale(1)'
+    icon.style.opacity = 0.8;
+    setTimeout(() => {
+        icon.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
+        icon.style.transform = 'translate(-50%,-50%) scale(0)'
+        icon.style.opacity = 0.8;
+    }, 2000);
+})
